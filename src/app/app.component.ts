ec7a92b90de9d191577349c8430cc53767e42028
sandeetukaramsa <sandeepsargar2000@gmail.com>
2022-12-09 12:01:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Payment';
+  userInput1 = "";
+
+ userInput2 = "";
+
+ userInput3 = "";
+
+ onInputReceiverName(vada1: Event) {
+
+  this.userInput1 = (vada1.target as HTMLInputElement).value
+
+  console.log(this.userInput1)
+
+ }
+
+ onInputPaymentDate(vada2: Event) {
+
+  this.userInput2 = (vada2.target as HTMLInputElement).value
+
+  console.log(this.userInput2)
+
+ }
+
+ onInputAmount(vada3: Event) {
+
+  this.userInput3 = (vada3.target as HTMLInputElement).value
+
+  console.log(this.userInput3)
+
+ 
+  }
 }
